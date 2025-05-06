3d5a37606637babf1bbf576ea6faf23df227a21b
Sharif-Miah <farhansharif1410@gmail.com>
2025-05-06 07:14:28 +0600

    Server action add user form in mongodb

---
+++ components/users/NewUserForm.jsx
@@ -0,0 +1,25 @@
+const NewUserForm = () => {
+  return (
+    <form>
+      <div>
+        <input
+          type='text'
+          name='name'
+          placeholder='Name'
+        />
+      </div>
+      <div>
+        <input
+          type='email'
+          name='email'
+          placeholder='Email'
+        />
+      </div>
+      <div>
+        <button type='submit'>Submit</button>
+      </div>
+    </form>
+  );
+};
+
+export default NewUserForm;
---
+++ components/users/UserList.jsx
@@ -0,0 +1,11 @@
+const UserList = () => {
+  return (
+    <div className='box-secondary mt-10'>
+      <h2 className='text-gray-800'>User List</h2>
+
+      <div className='box-1'>No user found!</div>
+    </div>
+  );
+};
+
+export default UserList;
